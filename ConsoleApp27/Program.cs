98ea6218f4a4b238fb40d2147bdaf4e4261ff8e1
efsanerecili <bd8epavpw@dode.edu.az>
2024-04-18 02:46:57 +0400

    added task6

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,36 @@
+﻿using Core;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                static void Main(string[] args)
+                {
+                    CustomList<int> myList = new CustomList<int>();
+
+                    myList.Add(1);
+                    myList.Add(2);
+                    myList.Add(3);
+                    myList.Add(4);
+                    myList.Add(5);
+
+                    myList.Remove(3);
+
+                    Console.WriteLine("Count: " + myList.Count);
+                    Console.WriteLine("Capacity: " + myList.Capacity);
+
+                    Console.WriteLine("Items:");
+                    myList.ForEach(Console.WriteLine);
+
+                    Console.ReadKey();
+                }
+            }
+
+
+        }
+    }
+    
+}
---
+++ Core/CustomList.cs
@@ -0,0 +1,119 @@
+﻿namespace Core
+{
+    public class CustomList<T>
+
+    {
+        private T[] _items;
+        private int _capacity;
+        private int _count;
+
+        public int Capacity => _capacity;
+        public int Count => _count;
+
+        public CustomList()
+        {
+            _capacity = 4;
+            _items = new T[_capacity];
+            _count = 0;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _count)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _count)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                _items[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_count == _capacity)
+            {
+                _capacity *= 2;
+                Array.Resize(ref _items, _capacity);
+            }
+            _items[_count++] = item;
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            foreach (T item in _items)
+            {
+                if (match(item))
+                    return item;
+            }
+            return default(T);
+        }
+
+        public List<T> FindAll(Predicate<T> match)
+        {
+            List<T> foundItems = new List<T>();
+            foreach (T item in _items)
+            {
+                if (match(item))
+                    foundItems.Add(item);
+            }
+            return foundItems;
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(_items[i], item))
+                {
+                    RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int RemoveAll(Predicate<T> match)
+        {
+            int removedCount = 0;
+            for (int i = 0; i < _count; i++)
+            {
+                if (match(_items[i]))
+                {
+                    RemoveAt(i);
+                    removedCount++;
+                    i--; // Bir önceki indekse dön
+                }
+            }
+            return removedCount;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            for (int i = index; i < _count - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _count--;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            foreach (var item in _items)
+            {
+                action(item);
+            }
+        }
+    }
+
+
+
+
+
+}
